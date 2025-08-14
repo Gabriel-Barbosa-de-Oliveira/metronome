83b8a433feb140a309faec4845f121134bb5b9b8
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-08-13 23:52:15 -0300

    Wip: Initial App Setup

--- custom-metronome/src/app/app.routes.ts
+++ custom-metronome/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Metronome } from './metronome/layout/metronome/metronome';
+import { NotFound } from './not-found/layout/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'metronome'
+  },
+  {
+    path: 'metronome',
+    component: Metronome
+  },
+  {
+    path: '**',
+    component: NotFound
+  }
+];
---
+++ custom-metronome/src/app/metronome/layout/metronome/metronome.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../../../shared/components/header/header';
+
+@Component({
+  selector: 'cm-metronome',
+  imports: [Header],
+  templateUrl: './metronome.html',
+  styleUrl: './metronome.scss'
+})
+export class Metronome {
+
+}
